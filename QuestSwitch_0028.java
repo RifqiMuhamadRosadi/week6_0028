befc2ea313059c5e09d497a89abbf955ba29c688
RifqiMuhamadRosadi <rifqimuhammm@gmail.com>
2025-11-05 18:28:39 +0700

    penambahan if else untuk menentukan predikat dan merapihkan tempat output IPK dan predikat

--- QuestSwitch_0028.java
+++ QuestSwitch_0028.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
-class QuestSwitch_0028{
+class QuestSwitch_0028 {
     public static void main(String[] args) {
         Scanner ns = new Scanner(System.in);
+        String predikat;
         int pilihanMatkul;
-        float nRataRata,nIPK,nStrukturData,nPBO,nBasisData,nAlgoritmaPemro,nRPL;
+        float nRataRata, nIPK, nStrukturData, nPBO, nBasisData, nAlgoritmaPemro, nRPL;
 
         System.out.println("Seleksi Pemilihan Asdos berdasarkan nilai akademik");
         System.out.println("Masukkan nilai akhir dari matakuliah dibawah ini");
@@ -24,11 +25,24 @@ class QuestSwitch_0028{
         System.out.print("Rekayasa Perangkat Lunak: ");
         nRPL = ns.nextFloat();
 
-        nRataRata = (nStrukturData+nPBO+nBasisData+nAlgoritmaPemro+nRPL)/5;
-        nIPK = nRataRata/25;
+        nRataRata = (nStrukturData + nPBO + nBasisData + nAlgoritmaPemro + nRPL) / 5;
+        nIPK = nRataRata / 25;
+
+        if (nIPK >= 3.75) {
+            predikat = "A (sangar baik)";
+        } else if (nIPK >= 3.5) {
+            predikat = "AB (baik sekali)";
+        } else if (nIPK >= 3) {
+            predikat = "B (baik)";
+        } else if (nIPK >= 2.5) {
+            predikat = "BC (cukup)";
+        } else {
+            predikat = "C (kurang)";
+        }
 
         System.out.println("");
-        System.out.println("IPK: "+nIPK);
+        System.out.println("IPK: " + nIPK);
+        System.out.println("predikat nilai: " + predikat);
 
     }
 }
