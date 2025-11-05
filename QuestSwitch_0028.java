5942173288295b9e42fd2108bc86e3776f77f83b
RifqiMuhamadRosadi <rifqimuhammm@gmail.com>
2025-11-05 16:42:43 +0700

    deklarasi variable (scanner, int, double)

--- QuestSwitch_0028.java
+++ QuestSwitch_0028.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 class QuestSwitch_0028{
     public static void main(String[] args) {
-        
+        Scanner ns = new Scanner(System.in);
+        int nStrukturData,nPBO,nBasisData,nAlgoritmaPemro,nRPL;
+        double nRataRata;
     }
 }
