15475d41163a170c2ab605d68290783a7634e683
RifqiMuhamadRosadi <rifqimuhammm@gmail.com>
2025-11-05 17:01:06 +0700

    menambahkan input nilai akhir

--- QuestSwitch_0028.java
+++ QuestSwitch_0028.java
@@ -5,5 +5,19 @@ class QuestSwitch_0028{
         Scanner ns = new Scanner(System.in);
         int nStrukturData,nPBO,nBasisData,nAlgoritmaPemro,nRPL;
         double nRataRata;
+
+        System.out.println("Seleksi Pemilihan Asdos berdasarkan nilai akademik");
+        System.out.println("Masukkan nilai akhir dari matakuliah dibawah ini");
+
+        System.out.print("Struktur Data: ");
+        nStrukturData = ns.nextInt();
+        System.out.print("Pemrograman Berorientasi Object");
+        nPBO = ns.nextInt();
+        System.out.print("Basis Data");
+        nBasisData = ns.nextInt();
+        System.out.print("Algoritma dan Pemrograman");
+        nAlgoritmaPemro = ns.nextInt();
+        System.out.print("Rekayasa Perangkat Lunak");
+        nRPL = ns.nextInt();
     }
 }
