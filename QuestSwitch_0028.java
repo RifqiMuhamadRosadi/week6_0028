f1b018cea1bd7f5a8bda106e478419be93826f83
RifqiMuhamadRosadi <rifqimuhammm@gmail.com>
2025-11-05 18:15:19 +0700

    menambahkan variable untuk nIPK dan membuat perhitungan nilai rata rata sampai IPK

--- QuestSwitch_0028.java
+++ QuestSwitch_0028.java
@@ -4,7 +4,7 @@ class QuestSwitch_0028{
     public static void main(String[] args) {
         Scanner ns = new Scanner(System.in);
         int pilihanMatkul;
-        float nRataRata,nStrukturData,nPBO,nBasisData,nAlgoritmaPemro,nRPL;
+        float nRataRata,nIPK,nStrukturData,nPBO,nBasisData,nAlgoritmaPemro,nRPL;
 
         System.out.println("Seleksi Pemilihan Asdos berdasarkan nilai akademik");
         System.out.println("Masukkan nilai akhir dari matakuliah dibawah ini");
@@ -23,5 +23,12 @@ class QuestSwitch_0028{
 
         System.out.print("Rekayasa Perangkat Lunak: ");
         nRPL = ns.nextFloat();
+
+        nRataRata = (nStrukturData+nPBO+nBasisData+nAlgoritmaPemro+nRPL)/5;
+        nIPK = nRataRata/25;
+
+        System.out.println("");
+        System.out.println("IPK: "+nIPK);
+
     }
 }
