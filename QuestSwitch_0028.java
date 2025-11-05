7b666eaa18508080758b7de3ff42dace14d1539f
RifqiMuhamadRosadi <rifqimuhammm@gmail.com>
2025-11-05 18:09:12 +0700

    perubahan tipedata dari int ke float

--- QuestSwitch_0028.java
+++ QuestSwitch_0028.java
@@ -3,21 +3,25 @@ import java.util.Scanner;
 class QuestSwitch_0028{
     public static void main(String[] args) {
         Scanner ns = new Scanner(System.in);
-        int nStrukturData,nPBO,nBasisData,nAlgoritmaPemro,nRPL;
-        double nRataRata;
+        int pilihanMatkul;
+        float nRataRata,nStrukturData,nPBO,nBasisData,nAlgoritmaPemro,nRPL;
 
         System.out.println("Seleksi Pemilihan Asdos berdasarkan nilai akademik");
         System.out.println("Masukkan nilai akhir dari matakuliah dibawah ini");
 
         System.out.print("Struktur Data: ");
-        nStrukturData = ns.nextInt();
-        System.out.print("Pemrograman Berorientasi Object");
-        nPBO = ns.nextInt();
-        System.out.print("Basis Data");
-        nBasisData = ns.nextInt();
-        System.out.print("Algoritma dan Pemrograman");
-        nAlgoritmaPemro = ns.nextInt();
-        System.out.print("Rekayasa Perangkat Lunak");
-        nRPL = ns.nextInt();
+        nStrukturData = ns.nextFloat();
+
+        System.out.print("Pemrograman Berorientasi Object: ");
+        nPBO = ns.nextFloat();
+
+        System.out.print("Basis Data: ");
+        nBasisData = ns.nextFloat();
+
+        System.out.print("Algoritma dan Pemrograman: ");
+        nAlgoritmaPemro = ns.nextFloat();
+
+        System.out.print("Rekayasa Perangkat Lunak: ");
+        nRPL = ns.nextFloat();
     }
 }
